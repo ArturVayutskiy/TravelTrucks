a5587a3eb2e4d1cb2dedab865a11dc8c580c923e
ArturVayutskiy <vayutskiy@gmail.com>
2024-10-12 23:34:01 +0300

    fix

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,55 +1,3 @@
-// import { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { useParams } from "react-router-dom";
-// import { fetchCamperById } from "../../redux/slice/camperSlice";
-// import Loader from "../../components/Loader/Loader";
-// import css from "./Details.module.css";
-// import CamperDetails from "../../components/DetailsPageComponents/CamperDetails/CamperDetails";
-// import Tabs from "../../components/Tabs/Tabs";
-// import BookForm from "../../components/DetailsPageComponents/BookForm/BookForm";
-
-// const Details = () => {
-//   const { id } = useParams();
-//   const dispatch = useDispatch();
-
-//   const {
-//     selectedCamper: camper,
-//     status,
-//     error,
-//   } = useSelector((state) => state.campers);
-
-//   useEffect(() => {
-//     dispatch(fetchCamperById(id));
-//   }, [dispatch, id]);
-
-//   if (status === "loading") {
-//     return (
-//       <div>
-//         <Loader />
-//       </div>
-//     );
-//   }
-
-//   if (status === "failed") {
-//     return <div>Error: {error}</div>;
-//   }
-
-//   if (!camper) {
-//     return <p>Camper is not found!</p>;
-//   }
-
-//   return (
-//     <div className={css.detailsContainer}>
-//       <CamperDetails camper={camper} />
-//       <div className={css.reviewsContainer}>
-//         <Tabs camper={camper} />
-//         <BookForm />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Details;
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -65,7 +13,7 @@ const Details = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
 
-  const [activeTab, setActiveTab] = useState("features"); // Активный таб
+  const [activeTab, setActiveTab] = useState("features");
 
   const {
     selectedCamper: camper,
