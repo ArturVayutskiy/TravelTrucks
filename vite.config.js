8e52d7a68530155c26eadc354da7ca23e1b1bfd3
ArturVayutskiy <vayutskiy@gmail.com>
2024-10-13 22:39:38 +0300

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { ViteStaticCopy } from "vite-plugin-static-copy";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     react(),
-    ViteStaticCopy({
+    viteStaticCopy({
       targets: [
         {
-          src: "_redirects",
-          dest: "",
+          src: "src/assets/icons/*",
+          dest: "assets/icons",
+        },
+        {
+          src: "src/assets/images/*",
+          dest: "assets/images",
         },
       ],
     }),
